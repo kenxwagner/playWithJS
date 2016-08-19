5b256f35b9f3eaaa9f1a6b4aefb4ece90e177ad8
Ken Wagner <ken_xwagner@hotmail.com>
2016-08-19 11:15:14 -0700

    more things
    just closures

--- functions-closure.js
+++ functions-closure.js
@@ -5,3 +5,25 @@ function someClosure() {
 	}
 	return closeX;
 }
+
+var someLocalFoo = someClosure();
+
+someLocalFoo();
+
+// creating more efficient functions
+
+function buildSomeTicketMaker (transport) {
+	return function ( name ) {
+		alert("Here is your ticket for riding the " + transport + ".\n" + 
+				"Welcome to Closer Transport, " + name + "!");
+	}
+}
+
+// create items to ride
+var getHorseRide = buildSomeTicketMaker("Horse");
+var getCarRide = buildSomeTicketMaker("Herbie the Love Bug");
+var getBalloonRide = buildSomeTicketMaker("Hot Air Balloon");
+
+getHorseRide("Rapunzel");
+getCarRide("Garfield");
+getBalloonRide("Mickey");
